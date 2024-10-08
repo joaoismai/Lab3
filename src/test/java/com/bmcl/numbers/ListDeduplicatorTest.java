465543e11685daae492b9a0e782b5e6a5f8d9295
José Gomes <you@example.com>
2024-10-08 22:25:02 +0100

    Lab3dia8ateEx5feito

--- src/test/java/com/bmcl/numbers/ListDeduplicatorTest.java
+++ src/test/java/com/bmcl/numbers/ListDeduplicatorTest.java
@@ -18,13 +18,31 @@ public class ListDeduplicatorTest {
         expected = Arrays.asList(1,2,4,5);
     }
 
+
     @Test
     public void deduplicate() {
-
-
-        ListDeduplicator deduplicator = new ListDeduplicator();
+        class StubListSorter implements GenericListSorter{
+            @Override public List<Integer> sort(List<Integer> list) {
+                return  Arrays.asList(1, 2, 4, 5);
+            }
+        }
+        StubListSorter sorter = new StubListSorter();
+        ListDeduplicator deduplicator = new ListDeduplicator(sorter);
         List<Integer> distinct = deduplicator.deduplicate(list);
-
+        Assertions.assertEquals(expected, distinct);
+    }
+    @Test
+    public void bug_deduplicate_8726() {
+        List<Integer> list = Arrays.asList(1,2,4,2);
+        List<Integer> expected = Arrays.asList(1,2,4);
+        class StubListSorter implements GenericListSorter{
+            @Override public List<Integer> sort(List<Integer> list) {
+                return  Arrays.asList(1, 2, 2, 4);
+            }
+        }
+        StubListSorter sorter = new StubListSorter();
+        ListDeduplicator deduplicator = new ListDeduplicator(sorter);
+        List<Integer> distinct = deduplicator.deduplicate(list);
         Assertions.assertEquals(expected, distinct);
     }
 }
